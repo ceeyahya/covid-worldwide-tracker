c145781c156ba80f575a32ea37083ecd582323ed
Yahya Chahine <chahineyahya@pm.me>
2020-09-18 12:28:40 +0100

    ✏️ Fixed typos in french translation

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -65,7 +65,7 @@ function dashboard({ t }) {
       </div>
       <div className="grid grid-cols-1 gap-5 mt-5 mb-16 lg:mb-24 sm:grid-cols-2 lg:grid-cols-3">
         {countryResults
-          // .filter((o) => o.countryInfo._id != 732)
+          .filter((o) => o.countryInfo._id != 732)
           .map((data) => {
             return (
               <div className="overflow-hidden bg-white rounded-lg shadow">
